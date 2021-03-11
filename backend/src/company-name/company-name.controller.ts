c0fc46e15744a3c9f1c6748742dd7dee0850a81e
Stanislav Yankovskiy <stanislav.yankovskiy@sirenltd.com>
2021-03-11 17:40:46 +0300

    small update

--- backend/src/company-name/company-name.controller.ts
+++ backend/src/company-name/company-name.controller.ts
@@ -1,15 +1,18 @@
 import {Controller, Get, HttpStatus, Query, Res} from '@nestjs/common';
 import {CompanyNameService} from "./company-name.service";
-import {CompanyIndustryEnum} from "./enums";
 
 @Controller('company-name')
 export class CompanyNameController {
     constructor(private companyNameService: CompanyNameService) { }
 
     @Get('/')
-    public async getAllCompanyNames(@Res() res, @Query('industry') industry) {
+    public async getAllCompanyNames(
+        @Res() res,
+        @Query('industry') industry
+    ) {
         const industryAsNumber = Number(industry);
         const companyNamesByIndustry = await this.companyNameService.findRandomByIndustry(industryAsNumber, 50);
+
         return res.status(HttpStatus.OK)
             .json(companyNamesByIndustry);
     }
