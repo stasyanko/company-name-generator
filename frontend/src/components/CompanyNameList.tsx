94585da9d31d42369d8242eba1ac3b42f9a204ef
Stanislav Yankovskiy <stanislav.yankovskiy@sirenltd.com>
2021-03-11 16:18:06 +0300

    update CompanyNameList

--- frontend/src/components/CompanyNameList.tsx
+++ frontend/src/components/CompanyNameList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Card, Col, Row} from "react-bootstrap";
+import {Card, Col, Form, Row} from "react-bootstrap";
 
 type companyName = {
     _id: string,
@@ -29,13 +29,23 @@ export class CompanyNameList extends React.Component<{}, CompanyNameListState> {
     render() {
         const companyNameList = this.state?.company_names?.map((companyName: companyName) => {
             return <Col xs lg="3">
-                <Card style={{width: '18rem'}}>
+                <Card style={{width: '18rem', marginTop: '8px'}} className={'text-center'}>
                     <Card.Body>{companyName.value}</Card.Body>
                 </Card>
             </Col>
         });
 
         return <>
+            <Form.Group controlId="exampleForm.SelectCustom">
+                <Form.Label>Select your industry</Form.Label>
+                <Form.Control as="select" custom>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </Form.Control>
+            </Form.Group>
             <Row>
                 {companyNameList}
             </Row>
